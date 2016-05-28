d437a4b9f92738297e6f295beb3b913598d44eb0
Ranjithadn <gowdaranjitha060@gmail.com>
2016-05-28 19:03:29 +0530

    Add files via upload

--- edittableplugin/js/table1.js
+++ edittableplugin/js/table1.js
@@ -1,17 +1,22 @@
 var host_api = "http://beta.zotey.com" ;
 var param_csv_file_id=1;
+var final_csv_data;
+
 
   function deletedbtn_handler(){
     var deleted_text_value=$(this).text();
+    var i=$(this).parents("tr").index();
     if(deleted_text_value=="no"){
       $(this).text("yes"); 
-      $(this).parents("tr").css('color', 'red');
+      $(this).parents("tr").css('color','red');
+      final_csv_data[i].deleted="yes";
     }
-    else if(deleted_text_value=="yes"){
+    else if(deleted_text_value=="yes"){ 
       $(this).text("no");
-      $(this).parents("tr").css('color', 'black');
+      $(this).parents("tr").css('color','black');
+      final_csv_data[i].deleted="no";
     } 
-  }
+  }//function deletedbtn_handler
 
     function table_data(){
     	$.ajax({
@@ -20,22 +25,21 @@ var param_csv_file_id=1;
               dataType: 'json',
               data: { csv_file_id: param_csv_file_id }, 
               success:function(data){
+                  final_csv_data=data.csv_data;
                   for(var i=0; i<data.csv_data.length; i++){
-                     var csv_table_row=document.createElement('tr')
+                     var csv_table_row=document.createElement('tr');
                      var csv_table_cell=document.createElement('td');
                      var csv_table_cell1=document.createElement('td');
                      var csv_table_cell3=document.createElement('td');
                      var csv_table_cell4=document.createElement('td');
                      var csv_table_cell5=document.createElement('td');
                      var csv_table_cell6=document.createElement('td');
-                     //var csv_table_cell7=document.createElement('td');
                      var csv_table_cell8=document.createElement('td');
                      var csv_table_cell9=document.createElement('td');
                      var csv_table_cell10=document.createElement('td');
                      var csv_table_cell11=document.createElement('td');
                      var csv_table_cell12=document.createElement('td');
                      var csv_table_cell13=document.createElement('td');
-                     //var csv_table_cell14=document.createElement('td');
                      var csv_table_cell15=document.createElement('td');
                      var csv_table_cell16=document.createElement('td');
                      var csv_cell_label1=document.createElement('label');
@@ -51,26 +55,12 @@ var param_csv_file_id=1;
                      if(deleted_value=="yes"){
                        $(csv_table_row).css("color", "red");
                      }
-                     //var csv_deleted_btn_options=document.createElement('option');
-                     
-
-                     //$(csv_deleted_btn_options).appendTo(csv_deleted_btn);
-                     //var csv_delete_btn=document.createElement('button');
-                     //$(csv_deleted_btn).text("Deleted");
-                     //$(csv_edit_btn).addClass("edit");
-                     //$(csv_delete_btn).text("Delete");
-                     //$(csv_delete_btn).addClass("delete");
-                     //$(csv_edit_btn).css({"backgroundColor":"#31b0d5", "color":"white", "border":"none",  "padding":"7px 14px", "borderRadius":"4px"});
-                     //$(csv_delete_btn).css({"backgroundColor":"#d9534f", "color":"white", "border":"none", "padding":"7px 14px", "borderRadius":"4px"});
-                     
                      //adding data attribute to tds 
                      $(csv_table_cell).attr('data-input','text-box');
                      $(csv_table_cell1).attr('data-input','text-box');
                      $(csv_table_cell3).attr('data-input','text-box');
                      $(csv_table_cell4).attr('data-input','text-box');
                      $(csv_table_cell5).attr('data-input','text-box');
-
-                     
                      //appending td's to tr
                      $(csv_table_cell).appendTo(csv_table_row);
                      $(csv_table_cell1).appendTo(csv_table_row);
@@ -78,14 +68,12 @@ var param_csv_file_id=1;
                      $(csv_table_cell4).appendTo(csv_table_row);
                      $(csv_table_cell5).appendTo(csv_table_row);
                      $(csv_table_cell6).appendTo(csv_table_row);
-                     //$(csv_table_cell7).appendTo(csv_table_row);
                      $(csv_table_cell8).appendTo(csv_table_row);
                      $(csv_table_cell9).appendTo(csv_table_row);
                      $(csv_table_cell10).appendTo(csv_table_row);
                      $(csv_table_cell11).appendTo(csv_table_row);
                      $(csv_table_cell12).appendTo(csv_table_row);
                      $(csv_table_cell13).appendTo(csv_table_row);
-                     //$(csv_table_cell14).appendTo(csv_table_row);
                      $(csv_table_cell15).appendTo(csv_table_row);
                      $(csv_table_cell16).appendTo(csv_table_row);
                      $(csv_cell_label1).appendTo(csv_table_cell);
@@ -94,8 +82,6 @@ var param_csv_file_id=1;
                      $(csv_cell_label4).appendTo(csv_table_cell4);
                      $(csv_cell_label5).appendTo(csv_table_cell5);
                      $(csv_deleted_btn).appendTo(csv_table_cell6);
-                     //$(csv_delete_btn).appendTo(csv_table_cell7);
-
                      //appending values to td's
                      $(csv_cell_label1).text(data.csv_data[i].test_name);
                      $(csv_cell_label2).text(data.csv_data[i].mrp);
@@ -108,10 +94,8 @@ var param_csv_file_id=1;
                      $(csv_table_cell11).text(data.csv_data[i].total_discount);
                      $(csv_table_cell12).text(data.csv_data[i].customer_discount);
                      $(csv_table_cell13).text(data.csv_data[i].zotey_commission);
-                     //$(csv_table_cell14).text(data.csv_data[i].highlight);
                      $(csv_table_cell15).text(data.csv_data[i].highlight_msg);
                      $(csv_table_cell16).text(data.csv_data[i].master_test_slug);
-                     
                      //highlighting row
                      $(csv_table_cell15).addClass("high");
                      var highlight_row=data.csv_data[i].highlight;
@@ -123,33 +107,35 @@ var param_csv_file_id=1;
                        $(this).attr('title', 'This is the hover-over text');
                       });
                      }
-                     
                      //appending tr to table
                      $(csv_table_row).appendTo('#csv_tabledata');
-                     
-                     //$("#csv_tabledata tr").on("click", "button.delete", delete_row_handler);
-                     //$("#csv_tabledata tr").on("click", ".cancel_btn",cancel_btn_handler);
-                     //$("#csv_tabledata tr").on("click", ".save",save_btn_handler);   
-                     
-
-                  }
+                  }//forloop
                     //updatetable button create
                      var update_div=document.createElement('div');
                      var update_table_btn=document.createElement('button');
                      $(update_table_btn).css({"background":"#1e5799", "border":"none", "width":"95px", "color":"white", "fontWeight":"800", "height":"35px", "borderRadius":"5px", "float":"right", "margin":"0px 60px 10px"});
                      $(update_table_btn).text("Update").appendTo(update_div);
                      $(update_div).appendTo("body");
-
-
-               }
+                     $(update_table_btn).on("click", update_data);
+               }//success
         }); //ajax
+    }//function table_data
 
-    }
-
-    
+    function update_data(){
+      
+      $.ajax({
+              url: host_api+"/m-api/admin/put_admin_lab_test_csv_data",
+              type:'POST',
+              dataType: 'json',
+              data: { csv_data: JSON.stringify(final_csv_data) }, 
+              success:function(data){
+                alert("success");
+              }//success
+      });//ajax
+    }//update_data
 
     $(document).ready(function(){
       table_data();
-    });   
+    });//ready function   
     
 
